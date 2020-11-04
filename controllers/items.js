88560ce2c81c0dd58abcb05637e62ffec2b6ea93
Opilane TTHK <opilane@tthk.ee>
2020-11-04 11:30:15 +0200

    Add db

--- controllers/items.js
+++ controllers/items.js
@@ -1,28 +1,44 @@
 const date = require("../generateDate.js");
-const Task = require("../models/task");
+const mongoose = require("mongoose");
+//const Task = require("../models/task");
 //let toDoList = [];
 
+const Task = mongoose.model("Task");
+
 exports.getMainPage = (req, res) => {
-    Task.fetchTasks(items =>{
-        let day = date.getDate();
-        res.render("index.ejs", {date: day, toDoItems: items});
+    let day = date.getDate();
+    Task.find((error, tasks) => {
+        if(!error){
+            res.render("index.ejs", {date: day, toDoItems: tasks});
+        }else{
+            console.log("Failed to retireve data: ",error);
+        }
     });
-    /*let weekday = date.getDate();
-    const itemsList = Task.fetchTasks();
-    /*let weekday = date.getWeekDay();
-    console.log(day);
-    res.render("index.ejs", {date: weekday, toDoItems: itemsList});*/
 };
 
 exports.postNewItem = (req, res) => {
-    const item = new Task(req.body.newTask);
-    item.saveTask();
-    /*let newTask = req.body.newTask;
-    toDoList.push(newTask);*/
-    res.redirect("/");
+    const item = req.body.newTask;
+    let newTask = new Task();
+    newTask.description = item;
+
+    newTask.save((error, response) => {
+        if(!error){
+            res.redirect("/");
+        }else{
+            console.log(error);
+        }
+    });
 };
+
 exports.deleteItem = (req, res) =>{
     console.log("Call from delete",req.body.checkbox);
-    Task.deleteItem(req.body.checkbox)
-    res.redirect('/')
+    const checkedItemId = req.body.checkbox;
+    Task.findByIdAndRemove(checkedItemId, function(error) {
+        if(!error){
+            console.log("Successfully deleted the item.");
+            res.redirect("/");
+        }else{
+            console.log(error);
+        }
+    });
 }
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+require("./models/db");
 const mainPage = require("./routes/main");
-const workPage = require("./routes/work")
+const workPage = require("./routes/work");
 const getError = require("./routes/404");
+
 
 const app = express();
 app.set("view engine", ejs);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/about", (req, res) =>{
-    res.render("about.ejs");
-});
+//app.get("/about", (req, res) =>{
+//    res.render("about.ejs");
+//});
 
 app.use(mainPage);
 app.use(workPage);
